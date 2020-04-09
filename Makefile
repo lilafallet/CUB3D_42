a1301883aa4f8816c374305758724bbbdf895a7d
Lila Fallet <lilafallet@gmail.com>
2020-04-09 16:40:01 +0200

    refacto

---
+++ Makefile
@@ -0,0 +1,88 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/03/09 16:08:02 by lfallet           #+#    #+#              #
+#    Updated: 2020/04/09 16:38:11 by lfallet          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = cub3d
+LIBDIR = ./libft
+LIB = $(LIBDIR)libft.a
+
+CFLAGS += -Wall
+CFLAGS += -Wextra
+ifeq ($(debug), 0)
+	CFLAGS += -g3
+else ifeq ($(debug), 1)
+	CFLAGS += -fsanitize=address
+	CFLAGS += -g3
+else ifeq ($(debug), 2)
+	CFLAGS += -fsanitize=address,undefined
+	CFLAGS += -g3
+else ifeq ($(debug), 3)
+	CFLAGS += -fsanitize=address,undefined
+	CFLAGS += -g3
+	CFLAGS += -Wpadded
+else ifeq ($(debug), 4)
+	CFLAGS += -fsanitize=address,undefined
+	CFLAGS += -g3
+	CFLAGS += -Wpadded
+	ifneq ($(CC), gcc)
+		CFLAGS += -Weverything
+	endif
+endif
+ifeq ($(err), 0)
+	CFLAGS += -g3
+else
+	CFLAGS += -Werror
+endif
+
+CC = clang
+
+INCLUDES = ./includes/
+INCLUDES_LIB = ./libft/includes/
+
+HEADER = $(INCLUDES)cub3d.h
+
+SRCS += main.c
+
+OBJ_DIR = ./objs/
+
+vpath %.c sources/
+
+OBJS = $(patsubst %.c, $(OBJ_DIR)%.o, $(SRCS))
+
+all : $(OBJ_DIR) $(NAME)
+
+$(OBJS): $(OBJ_DIR)%.o: %.c $(HEADER) Makefile
+	$(CC) $(CFLAGS) -c $<  -I $(INCLUDES) -I $(INCLUDES_LIB) -o $@
+
+$(NAME): $(LIB) $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -I$(INCLUDES) -I$(INCLUDES_LIB) -L./libft -lft -o $@
+
+$(OBJ_DIR):
+	mkdir $@
+
+$(LIB) : FORCE
+	$(MAKE) -C $(LIBDIR)
+
+FORCE :
+
+clean :
+	$(MAKE) clean -C $(LIBDIR)
+	$(RM) -R $(OBJ_DIR)
+
+fclean : clean
+	$(MAKE) fclean -C $(LIBDIR)
+	$(RM) $(NAME)
+
+re : fclean
+	$(MAKE)
+
+.PHONY: all clean fclean re FORCE
+#.SILENT:
---
+++ includes/cub3d.h
@@ -0,0 +1,195 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/10 20:41:48 by lfallet           #+#    #+#             */
+/*   Updated: 2020/04/09 16:36:21 by lfallet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# ifndef TRUE
+#  define TRUE 1
+# endif
+
+# ifndef FALSE
+#  define FALSE 0
+# endif
+
+# ifndef SUCCESS
+#  define SUCCESS 0
+# endif
+
+# ifndef FAILURE
+#  define FAILURE -1
+# endif
+
+# define LEN_ZERO 1
+
+# define CHAR_RESOLUTION 'R'
+# define STR_TEXTURE_NO "NO"
+# define STR_TEXTURE_SO "SO"
+# define STR_TEXTURE_WE "WE"
+# define STR_TEXTURE_EA "EA"
+# define STR_TEXTURE_SPR "S"
+# define CHAR_TEXTURE_SPR 'S'
+# define STR_COLOR "FC"
+# define CHAR_F_COLOR 'F'
+# define CHAR_C_COLOR 'C'
+# define STR_MAP "012NSWE"
+# define STR_TEXTURE "NOSOWEEA"
+# define FIRST_TEXTURE "NSWE"
+
+# define SPACE ' '
+# define TAB '\t'
+# define PATH "./"
+# define SIZE_PATH 2
+# define DOT '.'
+# define SLASH '/'
+# define PLUS '+'
+# define COMMA ','
+# define INDEX_TEXTURE 2
+
+# define ERROR_ARGUMENTS "Error of arguments: OUVRIR FENETRE(lire pdf)\n\n"
+# define WRONG_INFORMATIONS "Wrong informations : REGARDER QUOI FAIRE\n\n"
+
+# define BT_NO 	0x0000001
+# define BT_SO 	0x0000002
+# define BT_WE 	0x0000004
+# define BT_EA 	0x0000008
+# define BT_SPR 0x0000010
+
+# define BT_RESOLUTION 0x0000100
+# define BT_COLOR_F 0x0001000
+# define BT_COLOR_C 0x0002000
+# define BT_F_RED 0x0004000
+# define BT_F_GREEN 0x0008000
+# define BT_F_BLUE 0x0010000
+# define BT_C_RED 0x0020000
+# define BT_C_GREEN 0x0040000
+# define BT_C_BLUE 0x0080000
+# define BT_POSITION_N 0x0100000
+# define BT_POSITION_S 0x0200000
+# define BT_POSITION_W 0x0400000
+# define BT_POSITION_E 0x0800000
+
+# define IS_ERROR			0x0FF000000
+# define NB_ERROR			8
+# define ERR_USAGE			0
+# define ERROR_RESOLUTION	0x001000000
+# define ERROR_TEXTURE		0x002000000
+# define ERROR_COLOR		0x004000000
+# define ERROR_MAP			0x008000000
+# define ERROR_NO			0x010000000
+# define ERROR_SO			0x020000000
+# define ERROR_WE			0x040000000
+# define ERROR_EA			0x080000000
+
+# define ERR1				"Wrong resolution = voir ce qu'il faut faire\n"
+# define ERR2				"Wrong texture = voir ce qu'il faut faire\n"
+# define ERR3				"Wrong color = voir ce qu'il faut faire\n"
+# define ERR4				"Bad map = voir ce qu'il faut faire\n"
+
+# define BUFFER_SIZE 4096
+
+# define NB_TEXTURE 5
+# define NB_STATE 5
+# define NB_COLOR 3
+# define NB_RESOLUTION 2
+# define ALL 5
+# define NB_POSITION_MAP 1
+
+# define INDEX_RESOLUTION_X 0
+# define INDEX_RESOLUTION_Y 1
+
+# define MINUS '-'
+
+# define COLOR_C 1
+# define COLOR_F 2
+
+# include "libft.h"
+
+enum			e_state
+{
+	RESOLUTION, /*0*/
+	TEXTURE, /*1*/
+	SPRITE_TEXTURE, /*2*/
+	COLOR, /*3*/
+	MAP /*4*/
+};
+typedef struct  st_f_info
+{
+	unsigned long	resolution_x;
+	unsigned long	resolution_y;
+}				t_f_info;
+
+typedef struct	s_info
+{
+	size_t			index_map;
+	char			*str_texture[NB_TEXTURE];
+	char			*str_resolution[NB_RESOLUTION];
+	char			*str_text_spr;
+	char			*str_text_no;
+	char			*str_text_so;
+	char			*str_text_we;
+	char			*str_text_ea;
+	char			*str_color_f[NB_COLOR];
+	char			*str_color_c[NB_COLOR];
+	char			*str_map[BUFFER_SIZE];
+	char			str_position[NB_POSITION_MAP];
+	char			pad[7];
+}				t_info;
+
+typedef struct	s_state_machine
+{
+	char				*str_info_final;
+	char				*str_tmp_info_final;
+	unsigned long		information;
+	struct s_info		info;
+	struct st_f_info	final_info;
+	enum e_state		state;
+	char				pad[4];
+}				t_state_machine;
+
+typedef	int	(*t_function)(char *, t_state_machine *);
+
+/*int				main(int ac, char **av);
+int				first_parser(t_state_machine *machine, char *line);
+void			initialisation_info_final(int nb_line, char **dest, char *src);
+void			printf_errors(unsigned long flag, unsigned long line);
+int				ft_cub3d(t_state_machine *machine, int fd);
+void			ft_free(t_state_machine *machine, char *line);
+int				is_white_space(char *str);
+int				is_texture(char c);
+int				recup_resolution(char *str, t_state_machine *machine, int flag);
+int				recup_texture(char *str, t_state_machine *machine, int index);
+int				is_direction(char *str, size_t i);
+int				add_texture(t_state_machine *machine, int index, char *line);
+int				recup_color(char *str, char **color, int flag);
+int				what_position(char *str, char *position);
+int				parser_color_details(char *str, t_state_machine *machine,
+										char c, int flag);
+int				process_texture(int first_index, char *str, size_t i,
+								t_state_machine *machine);
+int				fill_str_resolution_color(char *str, char **str_axe_color,
+											int i, int flag);
+void			ft_free_tab(size_t size, char **str);
+int				hub_verification(char **str, int state,
+									t_state_machine *machine);
+int				hub_verification_map(t_state_machine *machine, char *line,
+									unsigned long nb_line);
+int				hub_recup_texture_resolution(char *str, size_t index,
+												t_state_machine *machine,
+												int flag);
+int				hub_recup_color(char *str, size_t index,
+									t_state_machine *machine, char c, int flag);
+unsigned long	atoul(const char *str);
+int				another_number(char *str, size_t i);
+void			what_state(t_state_machine *machine);*/
+
+#endif
