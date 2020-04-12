124fae9152092c0b6e9b4f5807f1dcc47f605bff
Lila Fallet <lilafallet@gmail.com>
2020-04-12 14:06:02 +0200

    change i = 0 vct_split

--- sources/parser/parser_texture.c
+++ sources/parser/parser_texture.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/11 14:53:41 by lfallet           #+#    #+#             */
-/*   Updated: 2020/04/11 22:58:41 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/04/12 13:23:12 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 void	what_bitwaze(t_state_machine *machine, size_t index)
 {
+	ft_printf("WHAT BITWAZE\n"); //	
 	if (index == 0)
 		machine->information |= BT_NO;
 	else if (index == 1)
@@ -35,14 +36,16 @@ int	texture_details(t_vector *texture, t_vector *vct, char *str_texture)
 	size_t	len;
 	int		count_path;
 
+	ft_printf("TEXTURE_DETAILS\n"); //	
 	len = ft_strlen(str_texture);
 	count_path = 0;
 	ret_str = ft_strnstr(vct_getstr(vct), str_texture, vct_getlen(vct));
 	vct_addstr(texture, ret_str + len);
 	vct_cpy(vct, texture);
+	ft_printf("texture->str 1 = %s\n", vct_getstr(texture)); //
 	while ((texture = vct_split(vct, "./", ALL_SEP)) != NULL)
 	{
-		ft_printf("texture->str = %s\n", vct_getstr(texture)); //
+		ft_printf("texture->str 2 = %s\n", vct_getstr(texture)); //
 		if (count_path == 1)
 		{
 			ft_printf("AFTER PATH\n");
@@ -80,6 +83,7 @@ int	texture_next_error(t_vector *vct, char **str_texture, size_t index, t_vector
 	char	*cpy_texture;
 	size_t	i;
 
+	ft_printf("TEXTURE_NEXT_ERROR\n"); //	
 	i = 0;
 	cpy_texture = ft_memdup(vct_getstr(vct), vct_getlen(vct));
 	len = vct_clen(vct, str_texture[index][0]);
@@ -100,7 +104,8 @@ int	is_texture(t_vector *texture, t_vector *vct, char **tab_texture)
 	int		ret;
 	char	*ret_str;
 	size_t	index;
-	
+
+	ft_printf("IS_TEXTURE\n"); //	
 	ret = TRUE;
 	ret_str = NULL;
 	index = 0;
