0b52e35147fc43e3887131f7be9b1cfc487d5b2b
Lila Fallet <lilafallet@gmail.com>
2020-06-03 19:51:58 +0200

    [DEV] fix image qui tressaute

--- sources/graphical/key.c
+++ sources/graphical/key.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/20 17:43:07 by lfallet           #+#    #+#             */
-/*   Updated: 2020/06/03 15:19:31 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/06/03 19:41:25 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,82 +15,33 @@
 int	keyrelease(int key, t_graph *gr)
 {
 	if (gr->mv.log & MV_UP && key == KMV_UP)
-	{
-		//ft_printf("DESACTIVE MV UP\n"); //
 		gr->mv.log &= ~MV_UP;
-	}
 	if (gr->mv.log & MV_DOWN && key == KMV_DOWN)
-	{
-		//ft_printf("DESACTIVE MV DOWN\n"); //
 		gr->mv.log &= ~MV_DOWN;
-	}
 	if (gr->mv.log & MV_LEFT && key == KMV_LEFT)
-	{
-		//ft_printf("DESACTIVE MV LEFT\n"); //
 		gr->mv.log &= ~MV_LEFT;
-	}
 	if (gr->mv.log & MV_RIGHT && key == KMV_RIGHT)
-	{
-		//ft_printf("DESACTIVE MV RIGHT\n"); //
 		gr->mv.log &= ~MV_RIGHT;
-	}
 	if (gr->mv.log & LK_LEFT && key == KLK_LEFT)
-	{
-		//ft_printf("DESACTIVE LK LEFT\n"); //
 		gr->mv.log &= ~LK_LEFT;
-	}
 	if (gr->mv.log & LK_RIGHT && key == KLK_RIGHT)
-	{
-		//ft_printf("DESACTIVE LK RIGHT\n"); //
 		gr->mv.log &= ~LK_RIGHT;
-	}
 	return (TRUE);
 }
 
 int	keypress(int key, t_graph *gr)
 {
-	int		mask;
-
-	mask = gr->mv.log;
 	if (key == KMV_UP && (gr->mv.log & MV_UP) == FALSE)
-	{
-		//ft_printf("ACTIVE MV UP\n"); //
 		gr->mv.log |= MV_UP;
-	}
 	if (key == KMV_DOWN && (gr->mv.log & MV_DOWN) == FALSE)
 		gr->mv.log |= MV_DOWN;
 	if (key == KMV_LEFT && (gr->mv.log & MV_LEFT) == FALSE)
-	{
-		//ft_printf("ACTIVE MV LEFT\n"); //
 		gr->mv.log |= MV_LEFT;
-	}
 	if (key == KMV_RIGHT && (gr->mv.log & MV_RIGHT) == FALSE)
-	{
-		//ft_printf("ACTIVE MV RIGHT\n"); //
 		gr->mv.log |= MV_RIGHT;
-	}
 	if (key == KLK_LEFT && (gr->mv.log & LK_LEFT) == FALSE)
-	{
-		//ft_printf("ACTIVE LK LEFT\n"); //
 		gr->mv.log |= LK_LEFT;
-	}
 	if (key == KLK_RIGHT && (gr->mv.log & LK_RIGHT) == FALSE)
-	{
-		//ft_printf("ACTIVE LK RIGHT\n"); //
 		gr->mv.log |= LK_RIGHT;
-	}
-	if (gr->mv.log & MV_UP || gr->mv.log & MV_LEFT || gr->mv.log & MV_RIGHT
-			|| gr->mv.log & MV_DOWN)
-	{
-		//ft_printf("ACTIVE MOOVE\n"); //
-		gr->mv.log |= MOOVE;
-	}
-	if (gr->mv.log & LK_LEFT || gr->mv.log & LK_RIGHT)
-	{
-		//ft_printf("ACTIVE LOOK\n"); //
-		gr->mv.log |= LOOK;
-	}
-	if (mask != gr->mv.log)
-		gr->update = TRUE;
 	return (TRUE);
 }
--- sources/graphical/moove.c
+++ sources/graphical/moove.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 10:20:42 by lfallet           #+#    #+#             */
-/*   Updated: 2020/06/03 15:19:31 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/06/03 19:48:40 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ static void	moove_up_down(t_graph *gr)
 {
 	if (gr->mv.log & MV_UP || gr->mv.log & MV_DOWN)
 	{
-		//ft_printf("MOOVE UP || DOWN\n\n"); //
 		gr->mv.new_posx = gr->mv.log & MV_UP ? gr->rting.posx + gr->rting.dirx
 							* SPEED_MV : gr->rting.posx - gr->rting.dirx
 							*SPEED_MV;
@@ -25,14 +24,12 @@ static void	moove_up_down(t_graph *gr)
 							*SPEED_MV;
 		if (gr->mv.log & MV_LEFT || gr->mv.log & MV_RIGHT)
 		{
-			//ft_printf("COMBINAISON UP/DOWN || LEFT/RIGHT\n\n"); //
 			gr->mv.comb_posx = gr->mv.new_posx;
 			gr->mv.comb_posy = gr->mv.new_posy;
 		}
 	}
 	if (gr->mv.log & MV_UP && gr->mv.log & MV_DOWN)
 	{
-		//ft_printf("MOOVE UP && DOWN\n\n"); //
 		gr->mv.new_posx = gr->rting.posx;
 		gr->mv.new_posy = gr->rting.posy;
 	}
@@ -44,7 +41,6 @@ static void	moove_left_right(t_graph *gr)
 	{
 		if (gr->mv.log & MV_UP || gr->mv.log & MV_DOWN)
 		{
-			//ft_printf("COMBINAISON LEFT/RIGHT || UP/DOWN\n\n"); //
 			gr->mv.new_posx = gr->mv.log & MV_RIGHT ? gr->mv.comb_posx
 								- gr->rting.diry * SPEED_MV : gr->mv.comb_posx
 								+ gr->rting.diry * SPEED_MV;
@@ -54,7 +50,6 @@ static void	moove_left_right(t_graph *gr)
 		}
 		else
 		{
-			//ft_printf("MOOVE LEFT || RIGHT\n\n"); //
 			gr->mv.new_posx = gr->mv.log & MV_RIGHT ? gr->rting.posx
 								- gr->rting.diry * SPEED_MV : gr->rting.posx
 								+ gr->rting.diry * SPEED_MV;
@@ -65,7 +60,6 @@ static void	moove_left_right(t_graph *gr)
 	}
 	if (gr->mv.log & MV_LEFT && gr->mv.log & MV_RIGHT)
 	{
-		//ft_printf("MOOVE LEFT && RIGHT\n\n"); //
 		gr->mv.new_posx = gr->rting.posx;
 		gr->mv.new_posy = gr->rting.posy;
 	}
@@ -73,6 +67,10 @@ static void	moove_left_right(t_graph *gr)
 
 void	init_moove_look(t_graph *gr, t_map *map)
 {
+	//static int	i = 1;
+
+	//ft_printf("RAYCASTING %d\n", i); //
+	//i++;
 	init_graph(gr, map);
 	process_window(gr);	
 }
@@ -83,30 +81,30 @@ int	moove(t_graph *gr)
 	double	tmp_dirx;
 	double	tmp_planecamx;
 	
-	if (gr->update == FALSE)
-		return (SUCCESS);
-	if (gr->mv.log & LOOK && gr->mv.log & MOOVE)
-		gr->update = TRUE;
-	else
-		gr->update = FALSE;
-	tmp_dirx = gr->rting.dirx;
-	tmp_planecamx = gr->rting.planecamx;
-	map = get_map(NULL);
-	if (gr->mv.log & LK_RIGHT || gr->mv.log & LK_LEFT)
-		gr->mv.log & LK_RIGHT ? look_right(gr, tmp_dirx, tmp_planecamx)
-									: look_left(gr, tmp_dirx, tmp_planecamx);	
-	if (gr->mv.log & MV_UP || gr->mv.log & MV_DOWN)
-		moove_up_down(gr);
-	if (gr->mv.log & MV_RIGHT || gr->mv.log & MV_LEFT)
-		moove_left_right(gr);
-	if (is_wall(gr, map) == FALSE)
+	if (gr->mv.log & MV_UP || gr->mv.log & MV_DOWN || gr->mv.log & MV_LEFT
+			|| gr->mv.log & MV_RIGHT || gr->mv.log & LK_LEFT
+			|| gr->mv.log & LK_RIGHT)
 	{
-		gr->rting.posx = gr->mv.new_posx;
-		gr->rting.posy = gr->mv.new_posy;
-		init_moove_look(gr, map);
+		tmp_dirx = gr->rting.dirx;
+		tmp_planecamx = gr->rting.planecamx;
+		map = get_map(NULL);
+		if (gr->mv.log & LK_RIGHT || gr->mv.log & LK_LEFT)
+			gr->mv.log & LK_RIGHT ? look_right(gr, tmp_dirx, tmp_planecamx)
+										: look_left(gr, tmp_dirx, tmp_planecamx);	
+		if (gr->mv.log & MV_UP || gr->mv.log & MV_DOWN)
+			moove_up_down(gr);
+		if (gr->mv.log & MV_RIGHT || gr->mv.log & MV_LEFT)
+			moove_left_right(gr);
+		if (is_wall(gr, map) == FALSE)
+		{
+			gr->rting.posx = gr->mv.new_posx;
+			gr->rting.posy = gr->mv.new_posy;
+			init_moove_look(gr, map);
+		}
+		else if (gr->mv.log & CAM)
+			init_moove_look(gr, map);
+		gr->mv.log &= ~CAM;
+		return (SUCCESS);
 	}
-	else if (gr->mv.log & CAM)
-		init_moove_look(gr, map);
-	gr->mv.log &= ~CAM;
-	return (SUCCESS);
+	return (FALSE);
 }
