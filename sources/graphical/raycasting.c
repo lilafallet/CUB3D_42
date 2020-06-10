9b869bed1de317ede5f6fd5c83f2d2c2b52e0626
Lila Fallet <lilafallet@gmail.com>
2020-06-10 15:02:21 +0200

    [FIX] map sans sprite

--- sources/graphical/raycasting.c
+++ sources/graphical/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/13 10:22:54 by lfallet           #+#    #+#             */
-/*   Updated: 2020/06/08 15:04:54 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/06/10 14:55:03 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -145,5 +145,6 @@ void	start_raycasting(t_map *map, t_graph *gr)
 		hub_draw(map, gr, x);
 		x++;
 	}
-	hub_sprite(map, gr);
+	if (gr->sp.nb_sprite != 0)
+		hub_sprite(map, gr);
 }
