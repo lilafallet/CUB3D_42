4b7e7d4aa9e0025efd48540fc14cbea7394815a6
Lila Fallet <lilafallet@gmail.com>
2020-06-10 16:18:33 +0200

    [FIX] RESOL > sizescreen

--- sources/graphical/start_graph.c
+++ sources/graphical/start_graph.c
@@ -6,7 +6,7 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 15:50:30 by lfallet           #+#    #+#             */
-/*   Updated: 2020/05/27 18:30:03 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/06/10 16:17:12 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 void	start_graph(t_map *map)
 {
 	t_graph	gr;
+	int	screenx;
+	int	screeny;
 
 	ft_bzero(&gr, sizeof(gr));
 	gr.win.mlx_ptr = mlx_init();
+	mlx_get_screen_size(gr.win.mlx_ptr, &screenx, &screeny);
+	//ft_printf("screenx = %d\n", screenx); //
+	//ft_printf("screeny = %d\n", screeny); //
+	if (map->recup.resolution[AXE_X] > screenx)
+		map->recup.resolution[AXE_X] = screenx;
+	if (map->recup.resolution[AXE_Y] > screeny)
+		map->recup.resolution[AXE_Y] = screeny;
 	gr.win.win_ptr = mlx_new_window(gr.win.mlx_ptr,
 							map->recup.resolution[AXE_X],
 							map->recup.resolution[AXE_Y], "Cub3d");
