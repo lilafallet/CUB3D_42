78c68e7696f27de7fabac8dba30e8fdeb200e8cb
Lila Fallet <lilafallet@gmail.com>
2020-06-13 16:57:31 +0200

    [NORME] start norme

--- sources/graphical/savemode.c
+++ sources/graphical/savemode.c
@@ -6,14 +6,14 @@
 /*   By: lfallet <lfallet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/11 15:40:00 by lfallet           #+#    #+#             */
-/*   Updated: 2020/06/13 16:19:19 by lfallet          ###   ########.fr       */
+/*   Updated: 2020/06/13 16:55:40 by lfallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdio.h>
 
-static void	int_to_char(int n, unsigned char *src)
+static void	int_to_char(unsigned char *src, int n)
 {
 	src[0] = (unsigned char)(n);
 	src[1] = (unsigned char)(n >> 8);
@@ -43,7 +43,8 @@ static void	image_bmp(t_graph *gr, t_map *map)
 			i++;
 		}
 		i = 0;
-		while (i < (4 - (map->recup.resolution[AXE_X] * 3) % 4) % 4)
+		while (i < (PIXOFFSET - (map->recup.resolution[AXE_X] * 3)
+						% PIXOFFSET) % PIXOFFSET)
 		{
 			write(gr->bmp.fd, &gr->bmp.pad, 1);
 			i++;
@@ -55,46 +56,25 @@ static void	image_bmp(t_graph *gr, t_map *map)
 void	savemode(t_map *map, t_graph *gr)
 {
 	int	image_size;
-	int	i;
 
-	i = 0;
 	image_size = 3 * map->recup.resolution[AXE_X] * map->recup.resolution[AXE_Y];
-	gr->bmp.size = 54 + image_size;
-	gr->bmp.image = malloc((sizeof(char) * image_size));
+	gr->bmp.size = HEADERSIZE + image_size;
 	//A PROTEGER
-	ft_memset(gr->bmp.image, 0, image_size);
 	gr->bmp.fd = open("screenshot.bmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	//A PROTEGER
-	while (i < 14)
-	{
-		gr->bmp.header[i] = 0;
-		i++;
-	}
-	gr->bmp.header[0] = 'B';
-	gr->bmp.header[1] = 'M';
-	gr->bmp.header[10] = 54;
-	i = 0;
-	while (i < 40)
-	{
-		gr->bmp.info[i] = 0;
-		i++;
-	}
-	gr->bmp.info[0] = 40;
-	gr->bmp.info[12] = 1;
-	gr->bmp.info[14] = 24;
-	i = 0;
-	while (i < 3)
-	{
-		gr->bmp.pad[i] = 0;
-		i++;	
-	}
-	int_to_char(gr->bmp.size, &gr->bmp.header[2]);
-	int_to_char(map->recup.resolution[AXE_X], &gr->bmp.info[4]);
-	int_to_char(map->recup.resolution[AXE_Y], &gr->bmp.info[8]);
-	write(gr->bmp.fd, gr->bmp.header, 14);
-	write(gr->bmp.fd, gr->bmp.info, 40);
+	ft_bzero(gr->bmp.header, HEADERSIZE - INFODATASIZE);
+	ft_bzero(gr->bmp.pad, 3);
+	gr->bmp.header[TYPEFILE0] = 'B';
+	gr->bmp.header[TYPEFILE0 + 1] = 'M';
+	gr->bmp.header[PIXOFFSET10] = HEADERSIZE;
+	gr->bmp.header[HEADERSIZE14] = INFODATASIZE;
+	gr->bmp.header[PLANECOLOR26] = PLANECOLOR;
+	gr->bmp.header[BPP28] = BPP;
+	int_to_char(&gr->bmp.header[FILESIZE2], gr->bmp.size);
+	int_to_char(&gr->bmp.header[IMGWIDTH18], map->recup.resolution[AXE_X]);
+	int_to_char(&gr->bmp.header[IMGHEIGHT22], map->recup.resolution[AXE_Y]);
+	write(gr->bmp.fd, gr->bmp.header, HEADERSIZE);
 	image_bmp(gr, map);
-	free(gr->bmp.image);
 	close(gr->bmp.fd);
 	exitred(gr);
 }
